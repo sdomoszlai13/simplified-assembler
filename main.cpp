69164db470944208082ec9b37993766d4b746702
sdomoszlai13 <sebastian.domoszlai@protonmail.com>
2023-03-14 14:24:27 +0100

    Added div() function.

--- main.cpp
+++ main.cpp
@@ -187,6 +187,47 @@ void sub(const string& line){
 }
 
 
+void div(const string& line){
+
+    // Division function
+
+    string arg_1 = line.substr(4, 2);
+	string arg_2 = line.substr(8);
+	regex reg {"(a|b|c)x"};
+
+	char typeof_arg_1 = typeInString(regs[arg_1]);
+	char typeof_arg_2;
+
+	if (regex_match(arg_2, reg)){
+        typeof_arg_2 = typeInString(regs[arg_2]);
+        arg_2 = regs[arg_2];
+	}
+
+	else{
+        typeof_arg_2 = typeInString(arg_2);
+	}
+
+
+	if (typeof_arg_1 == 's' || typeof_arg_2 == 's'){
+		throw invalid_argument("\nException: can't divide strings!\n");
+	}
+
+	else if (typeof_arg_1 == 'i' && typeof_arg_2 == 'i'){
+        if (arg_2 == "0")
+            throw invalid_argument("\nException: can't divide by zero!\n");
+        regs[arg_1] = std::to_string(stoi(regs[arg_1]) / stoi(arg_2));
+
+
+	}
+
+	else{
+        if (stof(arg_2) == 0)
+            throw invalid_argument("\nException: can't divide by zero!\n");
+		regs[arg_1] = to_string(stof(regs[arg_1]) / stof(arg_2));
+	}
+}
+
+
 int commandProcessor(const string& line){
 
     // Check if next line of file is valid assembler code. This is done with regex_match.
